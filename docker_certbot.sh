8cc847842f1fdc158ede893cb77bdca8755727d2
noah <noah.kim@sparkplus.co>
2025-08-26 12:47:29 +0900

    Feat: add domain

--- docker_certbot.sh
+++ docker_certbot.sh
@@ -4,6 +4,7 @@
 # 0 1 * * * /path/to/run.sh
 echo $(date "+%Y-%m-%d %H:%M:%S") "starting.." >> /root/cronlog-certbot.txt
 docker run --rm --name certbot -v '/etc/letsencrypt:/etc/letsencrypt' -v '/var/lib/letsencrypt:/var/lib/letsencrypt' -v '/home/opc/cloudflare.ini:/root/cloudflare.ini' certbot/dns-cloudflare certonly -d *.trigger.kr -d trigger.kr --dns-cloudflare --dns-cloudflare-credentials /root/cloudflare.ini --email ryan-dev@kakao.com --agree-tos --no-eff-email --force-renewal --dns-cloudflare-propagation-seconds 15
+docker run --rm --name certbot -v '/etc/letsencrypt:/etc/letsencrypt' -v '/var/lib/letsencrypt:/var/lib/letsencrypt' -v '/home/opc/cloudflare.ini:/root/cloudflare.ini' certbot/dns-cloudflare certonly -d *.saja-boys.com -d saja-boys.com --dns-cloudflare --dns-cloudflare-credentials /root/cloudflare.ini --email ryan-dev@kakao.com --agree-tos --no-eff-email --force-renewal --dns-cloudflare-propagation-seconds 15
 # docker run --rm --name certbot -v '/etc/letsencrypt:/etc/letsencrypt' -v '/var/lib/letsencrypt:/var/lib/letsencrypt' certbot/dns-cloudflare certonly -d *.DOMAIN2.COM --dns-cloudflare --dns-cloudflare-credentials ./cloudflare_cred.ini
 # docker run --rm --name certbot -v '/etc/letsencrypt:/etc/letsencrypt' -v '/var/lib/letsencrypt:/var/lib/letsencrypt' certbot/dns-cloudflare certonly -d *.DOMAIN3.COM --dns-cloudflare --dns-cloudflare-credentials ./cloudflare_cred.ini
 echo $(date "+%Y-%m-%d %H:%M:%S") "finish" >> /root/cronlog-certbot.txt
